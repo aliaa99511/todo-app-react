058609f9c8e8e3091e3c74b5f5c8da7210a7685a
aliaa99511 <aliaaattia7@gmail.com>
2021-01-08 16:06:02 +0200

    todo app react commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    newtask:" ",
+    todos:[
+        {text: 'add todo'}
+    ]
+  };
+
+  deleteApp=(index)=>{
+    const todos=[...this.state.todos];
+    todos.splice(index,1);
+    this.setState({
+      todos
+    })
+
+  }
+  updateTask=(event)=>{
+    this.setState({
+      newtask:event.target.value
+    })
+  }
+
+  addApp=(e)=>{
+    e.preventDefault();
+    const todos=[...this.state.todos];
+    todos.push({
+      text:this.state.newtask
+    });
+    this.setState({
+      todos,
+      newtask:" "
+    })
+  }
+
+
+  render() {
+
+    const {todos}=this.state
+    const todoapp=todos.length ?(
+        todos.map((todo,index)=>{
+      return(
+        <div className="mytext" key={index}>
+          {todo.text}
+          <button className=" mybtn  mybtn1" onClick={()=>{this.deleteApp(index)}}>delete</button>
+        </div>
+      )
+    })
+    ) : (
+      <p className="yay">you have no todos left , yay!</p>
+    )
+
+
+ 
+    return (
+      <div className="todo Container">
+        <h1 className="text-center myhead">todo App</h1>
+        <div className="mytodo">{todoapp } </div>
+
+        <label >add new  todo</label>
+        <form onSubmit={this.addApp}>
+          <input onChange={this.updateTask} value={this.state.newtask}/>
+          <button className="mybtn" type="submit" >add</button>
+        </form>
+
+
+      </div>
+
+    )
+  }
 }
 
 export default App;
+
+
+/*
+
+
+    isEdit:false,
+
+
+    let {isEdit} =this.state;
+
+
+
+toggleState=()=>{
+  let {isEdit}=this.state;
+  this.setState({
+    isEdit:!isEdit
+  })
+}
+
+
+renderUpdate=()=>{
+  return(
+    <form>
+      <input type="text" defaultValue={this.state.text}/>
+      <button>update</button>
+    </form>
+  )
+}
+  
+
+  <button className=" myedit" onClick={()=>{this.toggleState()}}>edit</button>
+
+
+<div>{ isEdit ? this.renderUpdate() : todoapp }</div>
+*/
